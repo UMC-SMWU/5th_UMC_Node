e47b0cbf7bbe0416f467e93b438273c65a7a6691
김민정 <mjeong@sookmyung.ac.kr>
2023-11-20 17:05:42 +0900

    [Feat(제리/김민정)] 에러 핸들링

--- 제리/config/response.status.js
+++ 제리/config/response.status.js
@@ -1,5 +1,20 @@
 import { StatusCodes } from 'http-status-codes';
 
 export const status = {
+    //success
     SUCCESS: { status: StatusCodes.OK, isSuccess: true, code: 2000, message: 'success!' },
+
+    //error
+    BAD_REQUEST: {
+        status: StatusCodes.BAD_REQUEST,
+        isSuccess: false,
+        code: 'COMMON001',
+        message: '잘못된 요청입니다.',
+    },
+    NOT_FOUND: {
+        status: StatusCodes.NOT_FOUND,
+        isSuccess: false,
+        code: 'COMMON005',
+        message: '요청한 페이지를 찾을 수 없습니다. 관리자에게 문의 바랍니다.',
+    },
 };
--- 제리/src/controllers/temp.controller.js
+++ 제리/src/controllers/temp.controller.js
@@ -1,7 +1,11 @@
 import { status } from '../../config/response.status.js';
-import { getTempData } from '../providers/temp.provider.js';
+import { getTempData, CheckFlag } from '../providers/temp.provider.js';
 import { response } from '../../config/response.js';
 
 export const tempTest = (req, res, next) => {
     res.send(response(status.SUCCESS, getTempData()));
+};
+
+export const tempException = (req, res, next) => {
+    return res.send(response(status.SUCCESS, CheckFlag(req.params.flag)));
 };
