8e25d85db75a80658319452e251b21621dfb84f1
김민정 <mjeong@sookmyung.ac.kr>
2023-11-20 23:25:15 +0900

    [Feat(제리/김민정)] 비동기 에러 핸들링

--- 제리/src/controllers/temp.controller.js
+++ 제리/src/controllers/temp.controller.js
@@ -1,6 +1,8 @@
 import { status } from '../../config/response.status.js';
 import { getTempData, CheckFlag } from '../providers/temp.provider.js';
 import { response } from '../../config/response.js';
+import { handleCall } from '../middlewares/temp.middleware.js';
+import { BaseError } from '../../config/error.js';
 
 export const tempTest = (req, res, next) => {
     res.send(response(status.SUCCESS, getTempData()));
@@ -9,3 +11,7 @@ export const tempTest = (req, res, next) => {
 export const tempException = (req, res, next) => {
     return res.send(response(status.SUCCESS, CheckFlag(req.params.flag)));
 };
+
+export const tempAsyncException = handleCall(async (req, res, next) => {
+    throw new BaseError(status.BAD_REQUEST);
+});
--- 제리/src/routes/temp.route.js
+++ 제리/src/routes/temp.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { tempTest, tempException } from '../controllers/temp.controller.js';
+import { tempTest, tempException, tempAsyncException } from '../controllers/temp.controller.js';
 
 export const tempRouter = express.Router();
 
 tempRouter.get('/test', tempTest);
 
 tempRouter.get('/exception/:flag', tempException);
+
+tempRouter.get('/exception', tempAsyncException);
