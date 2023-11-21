809f55ee316e3ba8dc70d82159e54c8d2788e25b
mjiw <113112060+mjiw@users.noreply.github.com>
2023-11-21 16:41:29 +0900

    [티모/박지우] 7주차 정리내용 제출. (#11)
    * chapter7
    
    * [Feat(티모/박지우)] 7주차 미션

---
+++ 티모/main.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql'); // mysql 모듈을 불러옵니다.
+
+// 커넥션을 정의합니다.
+// RDS Console 에서 본인이 설정한 값을 입력해주세요.
+var connection = mysql.createConnection({
+  host: "timo7-database.c8z9if4ndrhi.ap-northeast-2.rds.amazonaws.com",
+  user: "root",
+  password: "jiwoo8028",
+  database: "test"
+});
+
+// RDS에 접속합니다.
+connection.connect();
+
+connection.query('SELECT * from umc', function (error, results, fields) {
+  if (error) throw error;
+  console.log('users: ', results);
+});
+
+connection.end();
