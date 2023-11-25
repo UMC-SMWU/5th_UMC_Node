d7371ed05271e9cf10f248451695d31c8cbe5ed9
0-x-14 <0x14@daum.net>
2023-11-25 22:38:41 +0900

    [Feat(주피/이주영)] API 응답 통일 실습

---
+++ 주피/config/response.status.js
@@ -0,0 +1,9 @@
+// response.status.js
+
+import { StatusCodes } from "http-status-codes";
+
+export const status = {
+    // success
+    SUCCESS: {status: StatusCodes.OK, "isSuccess": true, "code": 2000, "message": "success!"},    
+    // 가독성을 위해 http-status-codes 사용
+}
