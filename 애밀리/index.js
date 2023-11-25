eefc1a212906984810278509a4de42ab57494db9
Shin Yejin <yejin0605@sookmyung.ac.kr>
2023-11-26 05:08:27 +0900

    [Feat(애밀리/신예진)] 에러 핸들링3

--- 애밀리/index.js
+++ 애밀리/index.js
@@ -1,3 +1,28 @@
+import express from 'express';
+import { tempRouter } from './src/routes/temp.route';
+
+const app = express();
+const port = 3000;
+
+// router setting
+app.use('/temp', tempRouter);
+/*
+// error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send(err.stack);
+});
+
+
+
+app.use((req, res, next) => {
+    const err = new BaseError(status.NOT_FOUND);
+    next(err);
+});
+*/
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
 
 app.use((err, req, res, next) => {
     // 템플릿 엔진 변수 설정
