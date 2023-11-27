e388f5fa1395bcb9440ca07984e4f7ca1a3e11f9
김민정 <mjeong@sookmyung.ac.kr>
2023-11-27 21:42:16 +0900

    [Feat(제리/김민정)] usersService 추가하기 #17

---
+++ 제리/services/users.service.js
@@ -0,0 +1,19 @@
+import { insertUser, setUserFoodType } from '../daos/user.dao';
+import { BaseError } from 'sequelize';
+import { status } from '../config/response.status';
+import { createUserResponseDTO } from '../dtos/user.dto';
+
+export const createUser = async (body) => {
+    try {
+        const user = await insertUser(body);
+        const userFoodTypes = [];
+        for (let i = 0; i < body.food_types.length; i++) {
+            const userFoodType = await setUserFoodType(user, body.food_types[i]);
+            userFoodTypes.push(userFoodType[0].dataValues.FoodTypeId);
+        }
+        return createUserResponseDTO(user, userFoodTypes);
+    } catch (err) {
+        console.log(err);
+        throw new BaseError(status.INTERNAL_SERVER_ERROR);
+    }
+};
