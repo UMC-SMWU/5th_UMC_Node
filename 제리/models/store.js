a38d6beacfc3bc854f2711eee9163551003a3d51
김민정 <mjeong@sookmyung.ac.kr>
2023-11-29 13:39:14 +0900

    [Feat(제리/김민정)] DB 생성 #18

---
+++ 제리/models/store.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+
+class Store extends Sequelize.Model {
+    static initiate(sequelize) {
+        Store.init(
+            {
+                name: {
+                    type: Sequelize.STRING(15),
+                    allowNull: false,
+                },
+                address: {
+                    type: Sequelize.STRING(30),
+                    allowNull: false,
+                },
+                region_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+            },
+            {
+                sequelize,
+                timestamps: true,
+                underscored: true,
+                modelName: 'Store',
+                tableName: 'store',
+                paranoid: true,
+                charset: 'utf8',
+                collate: 'utf8_general_ci',
+            }
+        );
+    }
+    static associate(db) {
+        db.Store.belongsTo(db.Region, { foreignKey: 'region_id' });
+    }
+}
+
+module.exports = Store;
