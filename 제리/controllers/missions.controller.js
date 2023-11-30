4d99c9c86e98ad7669b0e126a0f46ba92758c651
김민정 <mjeong@sookmyung.ac.kr>
2023-11-30 16:35:13 +0900

    [Feat(제리/김민정)] 컨트롤러 생성 #25

--- 제리/controllers/missions.controller.js
+++ 제리/controllers/missions.controller.js
@@ -1,8 +1,13 @@
 import { response } from '../config/response';
 import { status } from '../config/response.status';
 
-import { createMission } from '../services/missions.service';
+import { createMission, createUserMission } from '../services/missions.service';
 
 export const addMission = async (req, res, next) => {
     res.send(response(status.SUCCESS, await createMission(req.body)));
+};
+
+export const challengeMission = async (req, res, next) => {
+    const userId = 1;
+    res.send(response(status.SUCCESS, await createUserMission(userId, req.params)));
 };
