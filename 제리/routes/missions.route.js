caf34a24c73c888950705a9f4db975ad936ebdcc
김민정 <mjeong@sookmyung.ac.kr>
2023-11-30 16:34:38 +0900

    [Feat(제리/김민정)] 라우터 생성 #25

--- 제리/routes/missions.route.js
+++ 제리/routes/missions.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 
-import { addMission } from '../controllers/missions.controller.js';
+import { addMission, challengeMission } from '../controllers/missions.controller.js';
 
 export const missionsRouter = express.Router();
 
 missionsRouter.post('/', asyncHandler(addMission));
+missionsRouter.post('/:missionId', asyncHandler(challengeMission));
