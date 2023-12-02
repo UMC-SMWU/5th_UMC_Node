9240e60c54717cd6603ff064c24d6b8ddc28f624
0-x-14 <0x14@daum.net>
2023-12-03 01:39:30 +0900

    #30 [Feat(주피/이주영)] 회원가입 API 구현

---
+++ 주피/src/dtos/user.dto.js
@@ -0,0 +1,9 @@
+// sign in response DTO
+export const signinResponseDTO = (user, prefer) => {
+    const preferFood = [];
+    for (let i = 0; i < prefer[0].length; i++) {
+        preferFood.push(prefer[0][i].f_category_name);
+    }
+    return {"email": user[0].email, "name": user[0].name, "preferCategory": preferFood};
+    // 이메일, 이름, 선호 카테고리 전달
+}
