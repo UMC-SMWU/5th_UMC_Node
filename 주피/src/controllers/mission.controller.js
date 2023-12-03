a260f9b0e0ec0e018ac5dcba920da49a2b033e78
0-x-14 <0x14@daum.net>
2023-12-03 22:26:55 +0900

    [Feat(주피/이주영)] 서비스 생성 #41

--- 주피/src/controllers/mission.controller.js
+++ 주피/src/controllers/mission.controller.js
@@ -1,11 +1,11 @@
 import { response } from "../../config/response.js";
 import { status } from "../../config/response.status.js";
 
-import { challengeMission } from "../services/review.service.js";
+import { createUserMission } from "../services/review.service.js";
 
 export const missionChallenge = async (req, res, next) => {
     console.log("미션 도전을 요청하였습니다!");
     console.log("body:", req.body); // 값이 잘 들어오나 찍어보기 위한 테스트용
 
-    res.send(response(status.SUCCESS, await challengeMission(req.body)));
+    res.send(response(status.SUCCESS, await createUserMission(req.body)));
 }
---
+++ 주피/src/services/mission.service.js
@@ -0,0 +1,22 @@
+import { BaseError } from "../../config/error.js";
+import { status } from "../../config/response.status.js";
+import { addUserMissionResponseDTO } from "../dtos/mission.dto.js"
+// import { addUser, getUser, getUserPreferToUserID, setPrefer } from "../models/user.dao.js";
+// import { addReview, getReview } from "../models/review.dao.js";
+
+
+export const createUserMission = async (body) => {
+    const createUserMissionData = await addUserMission({
+        'user_id': body.user_id,
+        'mission_id': body.mission_id,
+        'status': body.status
+    });
+
+    if(createUserMissionData == -1){
+        // 존재하지 않는 미션일 경우 에러 발생
+        throw new BaseError(status.MISSION_DOESNT_EXISTS);
+    }else{
+        // else문 sql 생성 이후 수정해야 함
+        return addUserMissionResponseDTO(await getReview(createReviewData));
+    }
+}
