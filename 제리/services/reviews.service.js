e6ec491c1bb8abd0420fba96a0e46ed9a1dc2cee
김민정 <mjeong@sookmyung.ac.kr>
2023-12-03 13:32:54 +0900

    [Feat(제리/김민정)] 서비스 생성 #21

---
+++ 제리/services/reviews.service.js
@@ -0,0 +1,24 @@
+import { insertReview } from '../daos/reviews.dao';
+import { getStoreName } from '../daos/stores.dao';
+import { getUserName } from '../daos/users.dao';
+import { BaseError } from '../config/error';
+import { status } from '../config/response.status';
+import { createReviewResponseDTO } from '../dtos/create-review-response.dto';
+
+export const createReview = async (userId, body) => {
+    try {
+        const review = await insertReview(userId, body);
+        return createReviewResponseDTO(review, await getUserName(userId), await getStoreName(body.store_id));
+    } catch (err) {
+        console.log(err.name);
+        if (err.name === 'SequelizeValidationError' || err.name === 'SequelizeDatabaseError') {
+            throw new BaseError(status.BAD_REQUEST);
+        } else if (err.name === 'SequelizeUniqueConstraintError') {
+            throw new BaseError(status.DUPLICATE_ENTRY);
+        } else if (err.name === 'SequelizeForeignKeyConstraintError') {
+            throw new BaseError(status.NOT_FOUND);
+        } else {
+            throw new BaseError(status.INTERNAL_SERVER_ERROR);
+        }
+    }
+};
