2c36ac65fc334c4a162afcd5dbaf6ece947b2571
김민정 <mjeong@sookmyung.ac.kr>
2023-12-22 16:05:09 +0900

    [Feat(제리/김민정)] 내가 작성한 리뷰 조회 컨트롤러 함수 추가 #42

--- 제리/controllers/reviews.controller.js
+++ 제리/controllers/reviews.controller.js
@@ -1,9 +1,13 @@
 import { response } from '../config/response';
 import { status } from '../config/response.status';
 
-import { createReview } from '../services/reviews.service';
+import { createReview, findReviewByUserId } from '../services/reviews.service';
 
 export const addReview = async (req, res, next) => {
     const userId = 1;
     res.send(response(status.SUCCESS, await createReview(userId, req.body)));
+};
+
+export const fetchUserReviews = async (req, res, next) => {
+    res.send(response(status.SUCCESS, await findReviewByUserId(req.params)));
 };
