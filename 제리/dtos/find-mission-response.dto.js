d732df1ee75c88680e29eac7f51c086b178c59b6
김민정 <mjeong@sookmyung.ac.kr>
2023-12-22 21:36:36 +0900

    [Feat(제리/김민정)] 내가 진행 중인 미션 조회 DTO 수정 #44

--- 제리/dtos/find-mission-response.dto.js
+++ 제리/dtos/find-mission-response.dto.js
@@ -1,7 +1,7 @@
 export const findMissionResponseDTO = (missions) => {
     return missions.map((mission) => ({
-        reward: mission.reward,
-        store_name: mission['Store.name'],
-        target_amount: mission.target_amount,
+        reward: mission.reward || mission['Mission.reward'],
+        store_name: mission['Store.name'] || mission['Mission.Store.name'],
+        target_amount: mission.target_amount || mission['Mission.target_amount'],
     }));
 };
