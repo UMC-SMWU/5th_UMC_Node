1980e9b47c80c141eb2d37d1b953b91d7e8732c1
김민정 <mjeong@sookmyung.ac.kr>
2023-12-22 23:30:18 +0900

    [Feat(제리/김민정)] 특정 가게의 리뷰 조회 DTO 수정 #46

--- 제리/dtos/find-review-response.dto.js
+++ 제리/dtos/find-review-response.dto.js
@@ -1,8 +1,13 @@
 export const findReviewResponseDTO = (reviews) => {
     return reviews.map((review) => ({
         user_name: review['User.name'],
-        created_at: review.created_at,
+        created_at: formatDate(review.created_at),
         rating: review.rating,
         content: review?.content,
     }));
+};
+
+const formatDate = (date) => {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Intl.DateTimeFormat('kr', options).format(new Date(date)).replaceAll(' ', '').slice(0, -1);
 };
