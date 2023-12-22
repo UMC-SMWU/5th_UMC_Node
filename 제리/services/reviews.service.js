a76fe8088d8484b8a4a75141f756cbc2d5f008eb
김민정 <mjeong@sookmyung.ac.kr>
2023-12-22 16:05:49 +0900

    [Feat(제리/김민정)] 내가 작성한 리뷰 조회 서비스 함수 추가 #42

--- 제리/services/reviews.service.js
+++ 제리/services/reviews.service.js
@@ -1,9 +1,10 @@
-import { insertReview } from '../daos/reviews.dao';
+import { insertReview, selectReviewByUserId } from '../daos/reviews.dao';
 import { getStoreName } from '../daos/stores.dao';
 import { getUserName } from '../daos/users.dao';
 import { BaseError } from '../config/error';
 import { status } from '../config/response.status';
 import { createReviewResponseDTO } from '../dtos/create-review-response.dto';
+import { findReviewResponseDTO } from '../dtos/find-review-response.dto';
 
 export const createReview = async (userId, body) => {
     try {
@@ -20,5 +21,14 @@ export const createReview = async (userId, body) => {
         } else {
             throw new BaseError(status.INTERNAL_SERVER_ERROR);
         }
+    }
+};
+
+export const findReviewByUserId = async (param) => {
+    try {
+        const reviews = await selectReviewByUserId(param.userId);
+        return findReviewResponseDTO(reviews);
+    } catch (err) {
+        throw new BaseError(status.INTERNAL_SERVER_ERROR);
     }
 };
