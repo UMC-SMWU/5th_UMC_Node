c8a8465bd77c06f2ea562290a2101d8d5472bbb0
김민정 <mjeong@sookmyung.ac.kr>
2023-12-22 18:48:52 +0900

    [Feat(제리/김민정)] 특정 가게의 미션 조회 라우트 추가 #43

--- 제리/routes/missions.route.js
+++ 제리/routes/missions.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 
-import { addMission, challengeMission } from '../controllers/missions.controller.js';
+import { addMission, challengeMission, fetchStoreMissions } from '../controllers/missions.controller.js';
 
 export const missionsRouter = express.Router();
 
 missionsRouter.post('/', asyncHandler(addMission));
 missionsRouter.post('/:missionId', asyncHandler(challengeMission));
+missionsRouter.get('/stores/:storeId', asyncHandler(fetchStoreMissions));
