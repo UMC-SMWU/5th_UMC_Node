3da7cc0eae17f826cd4e5120993aa3c86e3044bd
김민정 <mjeong@sookmyung.ac.kr>
2023-12-22 18:50:36 +0900

    [Feat(제리/김민정)] 특정 가게의 미션 조회 DAO 함수 추가 #43

--- 제리/daos/missions.dao.js
+++ 제리/daos/missions.dao.js
@@ -1,4 +1,5 @@
-import { Mission } from '../models';
+import { Mission, Store } from '../models';
+import { Op } from 'sequelize';
 
 export const insertMission = async (data) => {
     return Mission.create({
@@ -15,4 +16,28 @@ export const getById = async (missionId) => {
 
 export const insertUserMission = async (mission, userId) => {
     return mission.addUser(userId);
+};
+
+export const selectMissionByStoreId = async (storeId, missionId, limit) => {
+    const condition = {
+        store_id: storeId,
+        ...(missionId !== undefined && { id: { [Op.lt]: missionId } }),
+    };
+    return buildSelectQuery(condition, limit);
+};
+
+const buildSelectQuery = (condition, limit) => {
+    return Mission.findAll({
+        raw: true,
+        where: condition,
+        include: [
+            {
+                model: Store,
+                attributes: ['name'],
+            },
+        ],
+        attributes: ['reward', 'target_amount'],
+        order: [['id', 'DESC']],
+        limit: limit,
+    });
 };
