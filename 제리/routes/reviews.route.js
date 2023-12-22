ca15f39b6ef214e346e37bb33cd7a5fdf64ebc66
김민정 <mjeong@sookmyung.ac.kr>
2023-12-22 16:02:42 +0900

    [Feat(제리/김민정)] 내가 작성한 리뷰 조회 라우트 추가 #42

--- 제리/routes/reviews.route.js
+++ 제리/routes/reviews.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 
-import { addReview } from '../controllers/reviews.controller.js';
+import { addReview, fetchUserReviews } from '../controllers/reviews.controller.js';
 
 export const reviewsRouter = express.Router();
 
 reviewsRouter.post('/', asyncHandler(addReview));
+
+reviewsRouter.get('/users/:userId', asyncHandler(fetchUserReviews));
