22c0ffa641da175386b8b4591824c42c847a58aa
김민정 <mjeong@sookmyung.ac.kr>
2023-12-22 23:28:52 +0900

    [Feat(제리/김민정)] 특정 가게의 리뷰 조회 라우트 추가 #46

--- 제리/routes/reviews.route.js
+++ 제리/routes/reviews.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 
-import { addReview, fetchUserReviews } from '../controllers/reviews.controller.js';
+import { addReview, fetchStoreReviews, fetchUserReviews } from '../controllers/reviews.controller.js';
 
 export const reviewsRouter = express.Router();
 
 reviewsRouter.post('/', asyncHandler(addReview));
-
+reviewsRouter.get('/stores/:storeId', asyncHandler(fetchStoreReviews));
 reviewsRouter.get('/users/:userId', asyncHandler(fetchUserReviews));
